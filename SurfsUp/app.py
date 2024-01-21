c3285a327a0f35c4cb78c7cabd1724887b13f1c2
kelcigriffin <kmgriffin89@gmail.com>
2024-01-21 10:50:49 -0600

    created base files for challenge

---
+++ SurfsUp/app.py
@@ -0,0 +1,41 @@
+# Import the dependencies.
+import numpy as np
+import datetime as dt
+import pandas as pd
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+#################################################
+# Database Setup
+#################################################
+
+# Create engine using the `hawaii.sqlite` database file
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+# Declare a Base using `automap_base()`
+Base = automap_base()
+# Use the Base class to reflect the database tables
+Base.prepare(autoload_with=engine)
+
+# Assign the measurement class to a variable called `Measurement` and
+# the station class to a variable called `Station`
+measurement = Base.classes['measurement']
+station = Base.classes["station"]
+
+# Create a session
+session = Session(engine)
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
